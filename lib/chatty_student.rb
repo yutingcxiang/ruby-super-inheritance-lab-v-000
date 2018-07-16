14414de751b462404754d91ae1b039cb8444b181
Christine Xiang <yutingcxiang@gmail.com>
2018-07-16 22:18:26 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,6 +5,15 @@ class ChattyStudent < Student
   end
   
   def raise_hand
-    super * 10
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
   end
 end
